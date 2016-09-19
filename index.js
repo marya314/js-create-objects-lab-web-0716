e76debd13c6a3149474a486ae4c992c09d494a81
marya314 <mary.boyd314@gmail.com>
2016-09-19 16:02:05 -0400

    added dog, cat, ferret, fish, bird constructor functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+function Dog(name, breed, age) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+}
+
+var Cat = function cat(name, breed, age) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+}
+
+var Ferret = function ferret(name, age, foodRequirements) {
+    this.name = name;
+    this.age = age;
+    this.foodRequirements = foodRequirements;
+}
+
+var Bird = function bird(name, breed) {
+    this.name = name;
+    this.breed = breed;
+}
+
+var Fish = function fish(name, species, waterConditions) {
+    this.name = name;
+    this.species = species;
+    this.waterConditions = waterConditions;
+}
